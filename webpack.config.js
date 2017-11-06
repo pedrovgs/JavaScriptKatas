4d6df11273d725fcd21ccc9da2bfb611dd9fb02f
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-06 11:13:52 -0300

    Add support for es2015

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         use: {
             loader: 'babel-loader',
             options: {
-                presets: ['env']
+                presets: ['es2015']
             }
         }
       }
