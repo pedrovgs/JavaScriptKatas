2037f55f2d216b2b4a42be0579de8627900a9766
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-07 11:03:12 -0300

    Solve kata 2

---
+++ test/es6katas/2-TemplateStringsMultilineTest.js
@@ -0,0 +1,41 @@
+// 2: template strings - multiline
+// To do: make all tests pass, leave the asserts unchanged!
+let assert = require("assert");
+describe("template string, can contain multiline content", function() {
+  it("a normal string can`t span across multiple lines", function() {
+    var normalString = `line1\nline2`;
+    assert.equal(normalString, "line1\nline2");
+  });
+
+  it("wrapped in backticks it can span over multiple lines", function() {
+    var templateString = `line1
+line2`;
+    assert.equal(templateString, "line1\nline2");
+  });
+
+  it("even over more than two lines", function() {
+    var multiline = `line 1
+line 2
+line 3
+line 4`;
+    assert.equal(multiline.split("\n").length, 4);
+  });
+
+  describe("and expressions inside work too", function() {
+    var x = 42;
+
+    it("like simple variables", function() {
+      let x = 42;
+      var multiline = `line 1
+      ${x}`;
+      assert.equal(multiline, "line 1\n      42");
+    });
+
+    it("also here spaces matter", function() {
+      let x = 42;
+      var multiline = `
+${x}`;
+      assert.equal(multiline, "\n42");
+    });
+  });
+});
