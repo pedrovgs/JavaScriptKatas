9f23bdf62a08de31dd7642e8f5c506f5ff8278f6
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-07 16:13:11 -0300

    Solve kata 15

---
+++ test/es6katas/15-DestructuringAssignTest.js
@@ -0,0 +1,32 @@
+// 15: destructuring - assign
+// To do: make all tests pass, leave the assert lines unchanged!
+let assert = require("assert");
+describe("assign object property values to new variables while destructuring", () => {
+  describe("for simple objects", function() {
+    it("use a colon after the property name, like so `propertyName: newName`", () => {
+      const { x: y } = { x: 1 };
+      assert.equal(y, 1);
+    });
+
+    it("assign a new name and give it a default value using `= <default value>`", () => {
+      const { x: y = 42 } = { y: 23 };
+      assert.equal(y, 42);
+    });
+  });
+
+  describe("for function parameter names", function() {
+    it("do it the same way, with a colon behind it", () => {
+      const fn = ({ x: y }) => {
+        assert.equal(y, 1);
+      };
+      fn({ x: 1 });
+    });
+
+    it("giving it a default value is possible too, like above", () => {
+      const fn = ({ x: y = 3 }) => {
+        assert.equal(y, 3);
+      };
+      fn({});
+    });
+  });
+});
