037160e8d2281195626e1d2d50ebc4a43007fc7f
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-07 16:11:11 -0300

    Solve kata 13

---
+++ test/es6katas/13-DestructuringDefaults.js
@@ -0,0 +1,30 @@
+// 13: destructuring - defaults
+// To do: make all tests pass, leave the assert lines unchanged!
+let assert = require("assert");
+describe("destructuring can also have default values", () => {
+  it("for an empty array", () => {
+    const [a = 1] = [];
+    assert.equal(a, 1);
+  });
+
+  it("for a missing value", () => {
+    const [b = 2] = [undefined, 1, 3];
+    assert.equal(b, 2);
+  });
+
+  it("in an object", () => {
+    const { b = 2 } = { a: 1 };
+    assert.equal(b, 2);
+  });
+
+  it("if the value is undefined", () => {
+    const { b = 2 } = { a: 1, b: void 0 };
+    assert.strictEqual(b, 2);
+  });
+
+  it("also a string works with defaults", () => {
+    const [a, b = 2] = "1";
+    assert.equal(a, "1");
+    assert.equal(b, 2);
+  });
+});
