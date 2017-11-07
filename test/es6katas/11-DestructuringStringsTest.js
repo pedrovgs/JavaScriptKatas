85466afadaf6deed62f4a0d02874f5b4950fe6af
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-07 13:14:07 -0300

    Solve kata 11

---
+++ test/es6katas/11-DestructuringStringsTest.js
@@ -0,0 +1,19 @@
+// 11: destructuring - string
+// To do: make all tests pass, leave the assert lines unchanged!
+let assert = require("assert");
+describe("destructuring also works on strings", () => {
+  it("destructure every character", () => {
+    let [a, b, c] = "abc";
+    assert.deepEqual([a, b, c], ["a", "b", "c"]);
+  });
+
+  it("missing characters are undefined", () => {
+    const [a, b, c] = "ab"; // eslint-disable-line
+    assert.equal(c, void 0);
+  });
+
+  it("unicode character work too", () => {
+    const [space, coffee] = "a☕"; // eslint-disable-line
+    assert.equal(coffee, "\u{2615}");
+  });
+});
