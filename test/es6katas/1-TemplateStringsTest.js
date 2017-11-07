41ca515caae603a3339eff726301509c9d56c1ae
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-07 10:48:10 -0300

    Solve kata 1

---
+++ test/es6katas/1-TemplateStringsTest.js
@@ -0,0 +1,41 @@
+// 1: template strings - basics
+// To do: make all tests pass, leave the asserts unchanged!
+let assert = require("assert");
+describe("a template string, is wrapped in ` (backticks) instead of ' or \"", function() {
+  describe("by default, behaves like a normal string", function() {
+    it("just surrounded by backticks", function() {
+      var str = `like a string`;
+      assert.equal(str, "like a string");
+    });
+  });
+
+  var x = 42;
+  var y = 23;
+
+  describe('can evaluate variables, which are wrapped in "${" and "}"', function() {
+    it('e.g. a simple variable "${x}" just gets evaluated', function() {
+      var evaluated = `x=${x}`;
+      assert.equal(evaluated, "x=" + x);
+    });
+
+    it("multiple variables get evaluated too", function() {
+      var evaluated = `${x}+${y}`;
+      assert.equal(evaluated, x + "+" + y);
+    });
+  });
+
+  describe('can evaluate any expression, wrapped inside "${...}"', function() {
+    it('all inside "${...}" gets evaluated', function() {
+      var evaluated = `${x + y}`;
+      assert.equal(evaluated, x + y);
+    });
+
+    it('inside "${...}" can also be a function call', function() {
+      function getDomain() {
+        return "tddbin.com";
+      }
+      var evaluated = `${getDomain()}`;
+      assert.equal(evaluated, "tddbin.com");
+    });
+  });
+});
