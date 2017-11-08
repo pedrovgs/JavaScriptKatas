642f6412c8ec2d0e03e672466dd7620439272f6f
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-08 17:52:37 -0300

    Solve kata 31

---
+++ test/es6katas/31-ArrayPrototypeFillTest.js
@@ -0,0 +1,31 @@
+// 31: array - `Array.prototype.fill` method
+// To do: make all tests pass, leave the assert lines unchanged!
+let assert = require("assert");
+describe("`Array.prototype.fill` can fill up an array with one value", () => {
+  it("`fill(0)` will populate `0` into each array element", function() {
+    const arr = new Array(3).fill(0);
+
+    assert.deepEqual(arr, [0, 0, 0]);
+  });
+
+  it("fill only changes content, adds no new elements", function() {
+    const arr = [].fill(0);
+
+    assert.deepEqual(arr, []);
+  });
+
+  it("second parameter to `fill()` is the position where to start filling", function() {
+    const fillPosition = 2;
+    const arr = [1, 2, 3].fill(42, fillPosition);
+
+    assert.deepEqual(arr, [1, 2, 42]);
+  });
+
+  it("third parameter is the position where filling stops", function() {
+    const fillStartAt = 1;
+    const fillEndAt = 2;
+    const arr = [1, 2, 3].fill(42, fillStartAt, fillEndAt);
+
+    assert.deepEqual(arr, [1, 42, 3]);
+  });
+});
