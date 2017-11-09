d45a7401708086a94f3030de708ceaaee4901a9d
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-09 10:36:33 -0300

    Solve kata 37

---
+++ test/es6katas/37-IteratorIterableArrayTest.js
@@ -0,0 +1,45 @@
+// 37: iterator/iterable - array.
+// The iterator protocol defines a standard way to produce a sequence of values (either finite or infinite).
+// read more at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols
+
+// To do: make all tests pass, leave the assert lines unchanged!
+let assert = require("assert");
+describe("array is a built-in iterable object", function() {
+  const arr = ["a", "B", "see"];
+
+  describe("the iterator", function() {
+    it("an array has an iterator, which is a function", function() {
+      const iterator = arr[Symbol.iterator];
+      const theType = typeof iterator;
+      const expected = "function";
+
+      assert.equal(theType, expected);
+    });
+
+    it("can be looped with `for-of`, which expects an iterable", function() {
+      let count = 0;
+      for (let a of arr) { // eslint-disable-line
+        count++;
+      }
+
+      assert.equal(count, arr.length);
+    });
+  });
+
+  describe("the iterator protocol", function() {
+    it("calling `next()` on an iterator returns an object according to the iterator protocol", function() {
+      const iterator = arr[Symbol.iterator]();
+      const firstItem = iterator.next();
+
+      assert.deepEqual(firstItem, { done: false, value: "a" });
+    });
+
+    it("the after-last element has done=true", function() {
+      const arr = [];
+      const iterator = arr[Symbol.iterator]();
+      const afterLast = iterator.next();
+
+      assert.deepEqual(afterLast, { done: true, value: void 0 });
+    });
+  });
+});
