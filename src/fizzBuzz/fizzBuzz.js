ee09b1c4318c9a49b465550d9c645a2e01c6fdda
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-10 10:57:19 -0300

    Simple unit tests finished. Let's practice property based

--- src/fizzBuzz/fizzBuzz.js
+++ src/fizzBuzz/fizzBuzz.js
@@ -1,5 +1,14 @@
 function fizzBuzz(value) {
-  return value.toString();
+  const isMultipleOf3 = isMultipleOf(value, 3);
+  const isMultipleOf5 = isMultipleOf(value, 5);
+  if (isMultipleOf3 && isMultipleOf5) return "FizzBuzz";
+  else if (isMultipleOf3) return "Fizz";
+  else if (isMultipleOf5) return "Buzz";
+  else return value.toString();
+}
+
+function isMultipleOf(x, y) {
+  return x % y == 0;
 }
 
 export default fizzBuzz;
--- test/fizzBuzz/FizzBuzz.spec.js
+++ test/fizzBuzz/FizzBuzz.spec.js
@@ -2,8 +2,26 @@ import fizzBuzz from "../../src/fizzBuzz/fizzBuzz";
 
 describe("FizzBuzz spec", () => {
   it("should return the value passed as String if it's not multiple of 3 or 5", () => {
-    const result = fizzBuzz(0);
+    const result = fizzBuzz(1);
 
-    expect(result).to.equal("0");
+    expect(result).to.equal("1");
+  });
+
+  it("should return Fixx if the value passed is multiple of three", () => {
+    const result = fizzBuzz(3);
+
+    expect(result).to.equal("Fizz");
+  });
+
+  it("should return Buzz if the value passed is multiple of five", () => {
+    const result = fizzBuzz(5);
+
+    expect(result).to.equal("Buzz");
+  });
+
+  it("should return FizzBUzz if the value passed is multiple of three and five", () => {
+    const result = fizzBuzz(15);
+
+    expect(result).to.equal("FizzBuzz");
   });
 });
