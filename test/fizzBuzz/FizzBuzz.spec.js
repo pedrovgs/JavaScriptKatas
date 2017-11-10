0a11ecfc20b2a4d8a9c52824f71cf9c2fefeef61
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-10 11:53:22 -0300

    Kata finished

--- test/fizzBuzz/FizzBuzz.spec.js
+++ test/fizzBuzz/FizzBuzz.spec.js
@@ -7,7 +7,7 @@ describe("FizzBuzz spec", () => {
     expect(result).to.equal("1");
   });
 
-  it("should return Fixx if the value passed is multiple of three", () => {
+  it("should return Fizz if the value passed is multiple of three", () => {
     const result = fizzBuzz(3);
 
     expect(result).to.equal("Fizz");
@@ -24,4 +24,55 @@ describe("FizzBuzz spec", () => {
 
     expect(result).to.equal("FizzBuzz");
   });
+
+  const arbitraryMultipleOfThree = jsc.suchthat(multipleOf(3), value => {
+    return value % 3 == 0 && value % 5 != 0;
+  });
+  const arbitraryMultipleOfFive = jsc.suchthat(multipleOf(5), value => {
+    return value % 5 == 0 && value % 3 != 0;
+  });
+  const arbitraryMultipleOfThreeAndFive = multipleOf(15);
+
+  function multipleOf(x) {
+    return jsc.integer.smap(value => Math.abs(value) * x);
+  }
+
+  function arbitraryNotMultipleOf(...values) {
+    return jsc.suchthat(
+      jsc.integer.smap(x => Math.abs(x)),
+      value => values.filter(x => value % x == 0).length == 0
+    );
+  }
+
+  jsc.property(
+    "values not multiple of 3 or 5 should be returned as a string",
+    arbitraryNotMultipleOf(3, 5),
+    value => {
+      return fizzBuzz(value) === value.toString();
+    }
+  );
+
+  jsc.property(
+    "values multiple of 3 should return Fizz",
+    arbitraryMultipleOfThree,
+    value => {
+      return fizzBuzz(value) === "Fizz";
+    }
+  );
+
+  jsc.property(
+    "values multiple of 5 should return Buzz",
+    arbitraryMultipleOfFive,
+    value => {
+      return fizzBuzz(value) === "Buzz";
+    }
+  );
+
+  jsc.property(
+    "values multiple of 3 and 5 should return FizzBuzz",
+    arbitraryMultipleOfThreeAndFive,
+    value => {
+      return fizzBuzz(value) === "FizzBuzz";
+    }
+  );
 });
