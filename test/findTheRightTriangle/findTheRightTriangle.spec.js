4c16238aa55059d4b8faa762ce5b7141596e7cea
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-13 10:02:04 -0300

    Finish right triangles kata

--- test/findTheRightTriangle/findTheRightTriangle.spec.js
+++ test/findTheRightTriangle/findTheRightTriangle.spec.js
@@ -1,5 +1,33 @@
+import findTheRightTriangles from "../../src/findTheRigthTriangle/findTheRightTriangle";
+
 describe("Find the right triangle spec", () => {
-  it("should", () => {
-      //TODO: Implement this
+  it("returns a non empty array because right triangles exist", () => {
+    const triangles = findTheRightTriangles();
+
+    expect(triangles).to.not.have.lengthOf(0);
+  });
+
+  it("every side of each triangle is less than or equal to 10", () => {
+    const triangles = findTheRightTriangles();
+
+    triangles.every(
+      triangle => triangle[0] <= 10 && triangle[1] <= 1 && triangle[2] <= 10
+    );
+  });
+
+  it("the perimeter of every triangle is equal to 24", () => {
+    const triangles = findTheRightTriangles();
+
+    triangles.every(triangle => triangle[0] + triangle[1] + triangle[2] === 24);
+  });
+
+  it("every triangle is a right triangle", () => {
+    const triangles = findTheRightTriangles();
+
+    triangles.every(
+      triangle =>
+        Math.pow(triangle[0], 2) + Math.pow(triangle[1], 2) ===
+        Math.pow(triangle[2], 2)
+    );
   });
 });
