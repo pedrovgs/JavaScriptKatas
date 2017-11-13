1117e61d24cc601bf501eb59faa36551dde5c145
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-13 10:38:01 -0300

    First implementation iteration

--- src/rockPaperScissors/rockPaperScissors.js
+++ src/rockPaperScissors/rockPaperScissors.js
@@ -1,7 +1,7 @@
 import { Enum } from "enumify";
 
 export class Gesture extends Enum {}
-Gesture.initEnum(["ROCK", "PAPER", "SCISSORS"]);
+Gesture.initEnum(["PAPER", "ROCK", "SCISSORS"]);
 
 export class Player {
   constructor(name, gesture) {
@@ -14,5 +14,18 @@ export class Result extends Enum {}
 Result.initEnum(["PLAYER_1_WINS", "PLAYER_2_WINS", "DRAW"]);
 
 export function whoWins(player1, player2) {
-  return Result.DRAW;
+  const match = [player1.gesture, player2.gesture];
+  const player1WinsResults = [
+    [Gesture.ROCK, Gesture.SCISSORS],
+    [Gesture.SCISSORS, Gesture.PAPER],
+    [Gesture.PAPER, Gesture.ROCK]
+  ];
+  const player2WinsResult = player2WinsResult.map(result => result.reverse());
+  if (player1WinsResults.includes(match)) {
+    return Result.PLAYER_1_WINS;
+  } else if (player2WinsResult.includes(match)) {
+    return Result.PLAYER_2_WINS;
+  } else {
+    return Result.DRAW;
+  }
 }
