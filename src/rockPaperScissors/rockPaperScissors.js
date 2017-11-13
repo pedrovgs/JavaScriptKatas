66a5c0fca16b6bf4441c9b7b8dec5ac889c5f26d
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-13 11:13:59 -0300

    Get the first test passing

--- src/rockPaperScissors/rockPaperScissors.js
+++ src/rockPaperScissors/rockPaperScissors.js
@@ -13,19 +13,34 @@ export class Player {
 export class Result extends Enum {}
 Result.initEnum(["PLAYER_1_WINS", "PLAYER_2_WINS", "DRAW"]);
 
+const player1WinsResults = [
+  [Gesture.ROCK, Gesture.SCISSORS],
+  [Gesture.SCISSORS, Gesture.PAPER],
+  [Gesture.PAPER, Gesture.ROCK]
+];
+const player2WinsResults = player1WinsResults.slice().reverse();
+
 export function whoWins(player1, player2) {
   const match = [player1.gesture, player2.gesture];
-  const player1WinsResults = [
-    [Gesture.ROCK, Gesture.SCISSORS],
-    [Gesture.SCISSORS, Gesture.PAPER],
-    [Gesture.PAPER, Gesture.ROCK]
-  ];
-  const player2WinsResult = player2WinsResult.map(result => result.reverse());
-  if (player1WinsResults.includes(match)) {
+  if (winsPlayer1(match)) {
     return Result.PLAYER_1_WINS;
-  } else if (player2WinsResult.includes(match)) {
+  } else if (winsPlayer2(match)) {
     return Result.PLAYER_2_WINS;
   } else {
     return Result.DRAW;
   }
+}
+
+function winsPlayer1(match) {
+  return wins(player1WinsResults, match);
+}
+
+function winsPlayer2(match) {
+  return wins(player2WinsResults, match);
+}
+
+function wins(winningGames, match) {
+  return winningGames.some(possibleGame =>
+    possibleGame.every((v, i) => v === match[i])
+  );
 }
