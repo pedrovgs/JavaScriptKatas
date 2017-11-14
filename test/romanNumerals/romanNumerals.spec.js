49d9f3867559fbdcfdd97adb365922571d2622f2
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-14 12:57:19 -0300

    Add some extra properties

--- test/romanNumerals/romanNumerals.spec.js
+++ test/romanNumerals/romanNumerals.spec.js
@@ -3,13 +3,42 @@ import { toRoman, toArabic } from "../../src/romanNumerals/romanNumerals";
 describe("Roman numerals spec", () => {
   jsc.property(
     "translate from arabic to numeral and back to roman should return the same value",
-    arbitraryNonZeroInt(),
+    arbitraryArabic(),
     arabic => {
       const romanNumeral = toRoman(arabic);
       return arabic === toArabic(romanNumeral);
     }
   );
-  function arbitraryNonZeroInt() {
-    return jsc.integer(1, 3000);
+
+  jsc.property(
+    "arabic numbers greater than zero returns non empty string as roman numeral",
+    arbitraryNonZeroArabic(),
+    arabic => {
+      return toRoman(arabic).length > 0;
+    }
+  );
+
+  jsc.property(
+    "roman numerals V, D or L can never be repeated",
+    arbitraryArabic(),
+    arabic => {
+      const romanNumeral = toRoman(arabic);
+      const containsAsMuchOneV = containsAsMuchOne(romanNumeral, "V");
+      const containsAsMuchOneD = containsAsMuchOne(romanNumeral, "D");
+      const containsAsMuchOneL = containsAsMuchOne(romanNumeral, "L");
+      return containsAsMuchOneV && containsAsMuchOneD && containsAsMuchOneL;
+    }
+  );
+
+  function containsAsMuchOne(string, value) {
+    return Array.of(string).filter(s => s === value).length <= 1;
+  }
+
+  function arbitraryArabic() {
+    return jsc.integer(0, 3000);
+  }
+
+  function arbitraryNonZeroArabic() {
+    return jsc.suchthat(arbitraryArabic(), x => x !== 0);
   }
 });
