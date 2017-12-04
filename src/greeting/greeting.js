9be42fc88753f1cf12adf236a56be860fddecb0d
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-12-04 19:50:50 +0100

    Finish requirement 3

--- src/greeting/greeting.js
+++ src/greeting/greeting.js
@@ -1,3 +1,11 @@
 export function greet(name = "my friend") {
-  return `Hello, ${name}.`;
+  if (isYelling(name)) {
+    return `HELLO ${name}!`;
+  } else {
+    return `Hello, ${name}.`;
+  }
+}
+
+function isYelling(name) {
+  return name === name.toUpperCase();
 }
--- test/greeting/greeting.spec.js
+++ test/greeting/greeting.spec.js
@@ -1,9 +1,13 @@
 import { greet } from "../../src/greeting/greeting";
 
 describe("Greetings kata", () => {
+  it("should say hello to bob if the name is bob", () => {
+    expect(greet("Bob")).to.deep.equal("Hello, Bob.");
+  });
+
   jsc.property(
     "says hello using the name passed as parameter",
-    jsc.string,
+    arbitraryLowerCaseNames(),
     name => {
       return `Hello, ${name}.` === greet(name);
     }
@@ -12,4 +16,23 @@ describe("Greetings kata", () => {
   it("should say hello to my friend if the input is not defined", () => {
     expect(greet()).to.deep.equal("Hello, my friend.");
   });
+
+  it("should say hello yelling to JERRY if he's yelling", () => {
+    expect(greet("JERRY")).to.deep.equal("HELLO JERRY!");
+  });
+
+  jsc.property(
+    "if the user of the lib is yelling, we should yell back",
+    jsc.asciistring.smap(name => name.toUpperCase()),
+    name => {
+      return `HELLO ${name}!` === greet(name);
+    }
+  );
 });
+
+function arbitraryLowerCaseNames() {
+  return jsc.suchthat(
+    jsc.asciinestring,
+    string => string !== string.toUpperCase()
+  );
+}
