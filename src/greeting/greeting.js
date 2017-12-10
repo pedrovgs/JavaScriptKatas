852fe56e9ff9ce19b7dca1f22aa40a9e846faa12
pedrovgs <pedrovicente.gomez@gmail.com>
2017-12-10 16:46:56 +0100

    Solve requirement number 7

--- src/greeting/greeting.js
+++ src/greeting/greeting.js
@@ -1,14 +1,37 @@
-export function greet(...name) {
-  if (isYelling(name)) {
-    return `HELLO ${name}!`;
+export function greet(...names) {
+  names = splitNamesIfNeeded(names);
+  if (isYelling(names)) {
+    return yellingGreet(names);
   } else {
-    return `Hello, ${joinNames(name)}.`;
+    const regularNames = names.filter(name => !isYelling(name));
+    const yellingNames = names.filter(name => isYelling(name));
+    if (regularNames.length > 0 && yellingNames.length > 0) {
+      return yellingAndRegularGreet(regularNames, yellingNames);
+    } else {
+      return regularGreet(names);
+    }
   }
 }
 
+function regularGreet(names) {
+  return `Hello, ${joinNames(names)}.`;
+}
+
+function yellingGreet(names) {
+  return `HELLO ${names}!`;
+}
+
+function yellingAndRegularGreet(regularNames, yellingNames) {
+  return `${regularGreet(regularNames)} AND ${yellingGreet(yellingNames)}`;
+}
+
 function isYelling(names) {
-  if (typeof names !== "undefined" && names.length > 0) {
+  let isDefined = typeof names !== void 0;
+  let isAnArray = Array.isArray(names);
+  if (isDefined && isAnArray && names.length > 0) {
     return names.every(name => name === `${name}`.toUpperCase());
+  } else if (isDefined && !isAnArray) {
+    return names === names.toUpperCase();
   } else {
     return false;
   }
@@ -25,4 +48,13 @@ function joinNames(names) {
     const firstNames = names.slice(0, names.length - 1).join(", ");
     return `${firstNames}, and ${names[names.length - 1]}`;
   }
+}
+
+function splitNamesIfNeeded(names) {
+  if (typeof names === void 0 || names.length === 0) {
+    return names;
+  }
+  return names.map(name => name.split(",")).reduce(function(a, b) {
+    return a.concat(b);
+  });
 }
--- test/greeting/greeting.spec.js
+++ test/greeting/greeting.spec.js
@@ -2,23 +2,23 @@ import { greet } from "../../src/greeting/greeting";
 
 describe("Greetings kata", () => {
   it("should say hello to bob if the name is bob", () => {
-    expect(greet("Bob")).to.deep.equal("Hello, Bob.");
+    expect(greet("Bob")).to.equal("Hello, Bob.");
   });
 
   jsc.property(
     "says hello using the name passed as parameter",
-    arbitraryLowerCaseNames(),
+    arbitraryLowerCaseRegularName(),
     name => {
       return `Hello, ${name}.` === greet(name);
     }
   );
 
   it("should say hello to my friend if the input is not defined", () => {
-    expect(greet()).to.deep.equal("Hello, my friend.");
+    expect(greet()).to.equal("Hello, my friend.");
   });
 
   it("should say hello yelling to JERRY if he's yelling", () => {
-    expect(greet("JERRY")).to.deep.equal("HELLO JERRY!");
+    expect(greet("JERRY")).to.equal("HELLO JERRY!");
   });
 
   jsc.property(
@@ -30,7 +30,7 @@ describe("Greetings kata", () => {
   );
 
   it("should say hello to every member in the group of two people if we greet more than one person", () => {
-    expect(greet("Jill", "Jane")).to.deep.equal("Hello, Jill and Jane.");
+    expect(greet("Jill", "Jane")).to.equal("Hello, Jill and Jane.");
   });
 
   jsc.property(
@@ -38,7 +38,7 @@ describe("Greetings kata", () => {
     arbitraryListOfNamesGreaterThan(2),
     names => {
       let result = greet(...names);
-      let containsOxfordComa = result.indexOf(", and") != -1;
+      let containsOxfordComa = result.indexOf(", and") !== -1;
       let containsAsManyComasAsNames =
         result.split(", ").length - 1 === names.length;
       return containsOxfordComa && containsAsManyComasAsNames;
@@ -47,20 +47,30 @@ describe("Greetings kata", () => {
 
   it("should say hello to my three friends using Oxford coma", () => {
     let result = greet("Amy", "Brian", "Charlotte");
-    expect(result).to.deep.equal("Hello, Amy, Brian, and Charlotte.");
+    expect(result).to.equal("Hello, Amy, Brian, and Charlotte.");
+  });
+
+  it("should say hello to a group of friends when someone is yelling and some others not", () => {
+    let result = greet("Amy", "BRIAN", "Charlotte");
+    expect(result).to.equal("Hello, Amy and Charlotte. AND HELLO BRIAN!");
+  });
+
+  it("should split names containing , as two names", () => {
+    let result = greet("Amy", "Brian", "Char,lotte");
+    expect(result).to.equal("Hello, Amy, Brian, Char, and lotte.");
   });
 });
 
-function arbitraryLowerCaseNames() {
+function arbitraryLowerCaseRegularName() {
   return jsc.suchthat(
     jsc.asciinestring,
-    string => `${string}`.toUpperCase() !== string
+    string => `${string}`.toUpperCase() !== string && string.indexOf(",") === -1
   );
 }
 
 function arbitraryListOfNamesGreaterThan(minimumListSize) {
   return jsc.suchthat(
-    jsc.nearray(arbitraryLowerCaseNames()),
+    jsc.nearray(arbitraryLowerCaseRegularName()),
     names => names.length > minimumListSize
   );
 }
